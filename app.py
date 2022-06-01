5a2c613923cd095499afdc40c306f48bb5c8a5df
fenil patel <fenilpatel12997@gmail.com>
2022-06-01 19:27:34 +0530

    learning connecting db on heroku 1st try

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import database
 
 #initialize flask app
 app = Flask(__name__)
-# con,cur = database.tbl_con() 
+con,cur = database.tbl_con() 
 
 
 #Set home page
@@ -21,36 +21,21 @@ def home():
     # Project  = request.form['Project']
     # contact = request.form['Query']
 def insert():
-    return render_template('index1.html',title='Home')
-    # try:
-        # E = input("Enter your emp id")
-        # N   = input("Enter Your Name")
-        # P = input("Enter your project name")
-        # Q  = input("Write your query")
-    #     Emp_Id = request.form['Emp_Id']
-    #     Name = request.form['Name']
-    #     Project  = request.form['Project']
-    #     Query = request.form['Query']
-    #     data = (Emp_Id,Name,Project,Query)
-    #     Qry  = "INSERT INTO employee(Emp_id,Name,Project,Query) VALUES (%s,%s,%s,%s)"
-    #     #insert data to table
-    #     cur.execute(Qry,data)
-    #     return "data inserted successfully.."
-    # finally:
-    #     con.commit()
-    #     cur.close()
-    #     con.close()
+    
+    try:
+    
+        Emp_Id = request.form['Emp_Id']
+        Name = request.form['Name']
+        Project  = request.form['Project']
+        Query = request.form['Query']
+        data = (Emp_Id,Name,Project,Query)
+        Qry  = "INSERT INTO employee(Emp_id,Name,Project,Query) VALUES (%s,%s,%s,%s)"
+        #insert data to table
+        cur.execute(Qry,data)
+        return "data inserted successfully.."
+    finally:
+        con.commit()
+        cur.close()
+        con.close()
         
-
-    # pred = pd.DataFrame(data={'month':[float(month)],'emp.var.rate':[float(emp)] ,'cons.conf.idx':[float(cons)],'contact':[float(contact)],
-    #                    'housing':[float(housing)],'euribor3m':[float(euribor3m)],'default': [float(default)]})
-    # prediction = model.predict(pred)
-    # output = prediction[0] 
-    # if output > 0:
-    #     output="Genuine"
-    #     return render_template('prediction.html', prediction_text=f'Prediction For Applied Person is {output} Person.')
-    # else:
-    #     output = "Fraud"
-    #     return render_template('prediction.html', prediction_text=f'Prediction For Applied Person is {output} Person!!')
-# employee.insert()   
- # or True             
+             
--- database.py
+++ database.py
@@ -1,18 +1,27 @@
 import psycopg2 as db
+import os
+from urllib.parse import  urlparse as url
 from  psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 #connection url
-# DATABASE_URL = postgres://wlbyqskowiojck:1931935b2ed9e1bfe8eb3d7282e7ac411efc3d6f0208dabc2421d03591ec80d6@ec2-54-165-178-178.compute-1.amazonaws.com:5432/ddv11fq19f7frl
+# DATABASE_URL = 'postgres://wlbyqskowiojck:1931935b2ed9e1bfe8eb3d7282e7ac411efc3d6f0208dabc2421d03591ec80d6@ec2-54-165-178-178.compute-1.amazonaws.com:5432/ddv11fq19f7frl'
+URL = url(os.environ.get('DATABASE_URL'))
+usr,pword,dbase,host= URL.username,URL.password,URL.path[1:],URL.hostname
 #connection variable for creating database
 def db_con():
     con = db.connect(host='localhost',user='fenil',password="fenilpatel@123",port=5432)
     con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     return con
-#connection variable for creating table    
+#connection variable for creating table on heroku   
 def tbl_con():
-    con = db.connect(database="Portal",user="fenil",password="fenilpatel@123",host="localhost",port=5432)
+    con = db.connect(user=usr,password=pword,host=host,database=dbase,)
     cur = con.cursor()
     return con,cur
+#connection variable for creating table on local    
+# def tbl_con():
+#     con = db.connect(database="Portal",user="fenil",password="fenilpatel@123",host="localhost",port=5432)
+#     cur = con.cursor()
+#     return con,cur
 
 #creating new database 
 def database():
@@ -45,4 +54,4 @@ def table():
         con.close()
         
 # database()
-# table()
+table()
