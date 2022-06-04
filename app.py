baec270da952af3ee4cd862646904bcbc5ee0463
fenil patel <fenilpatel12997@gmail.com>
2022-06-04 12:46:11 +0530

    Update and Delete Works in Local Env

--- app.py
+++ app.py
@@ -70,7 +70,60 @@ def find():
     con.close()
     cur.close()
     con.close()
-         
+    
+@app.route('/delete',methods=['GET','POST'])
+def delete():
+    try:
+    
+        #create a connection to database
+        con,cur = tbl_con()
+        qry1 =  "DELETE FROM employee where emp_id=%s"
+        qry2 = "SELECT * FROM employee"
+        Emp_Id = request.form['Emp']
+        data = (Emp_Id,)
+        #create a new table
+        cur.execute(qry1,data)
+        con.commit()
+        rm = cur.rowcount
+        cur.execute(qry2)
+        tbl = cur.fetchall()
+        con.commit()
+        
+        return render_template('view.html',rm=rm,tbl=tbl)
+    except db.DatabaseError as e:
+        print(e)
+    con.close()
+    cur.close()
+    con.close()
+    
+@app.route('/update',methods=['GET','POST'])
+def update():
+    try:
+    
+        #create a connection to database
+        con,cur = tbl_con()
+        qry1 =  "UPDATE employee SET Emp_Id=%s,Name=%s,Project=%s,Query=%s where Emp_Id=%s"
+        qry2 = "SELECT * FROM employee"
+        Emp_Id = request.form['Emp_Id']
+        Name = request.form['Name']
+        Project  = request.form['Project']
+        Query = request.form['Query']
+        Old_Emp_Id = request.form['Old_Emp_Id']
+        data = (Emp_Id,Name,Project,Query,Old_Emp_Id)
+    
+        cur.execute(qry1,data)
+        con.commit()
+        
+        cur.execute(qry2)
+        tbl = cur.fetchall()
+        con.commit()
+        
+        return render_template('view.html',tbl=tbl)
+    except db.DatabaseError as e:
+        print(e)
+    con.close()
+    cur.close()
+    con.close()
 @app.errorhandler(HTTPException)
 def handle_exception(e):
     if isinstance(e,HTTPException):
